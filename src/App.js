359500aa1dcb763a992a9a089365f1b8ea693da7
amitjhacse <akj.cs.20@gmail.com>
2021-02-25 10:00:17 +0530

    Login form added

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import React from "react";
 import { ChatEngine, ChatFeed } from "react-chat-engine";
 import ChatFeeds from "./components/ChatFeeds";
+import LoginForm from "./components/LoginForm";
 
 import "./App.css";
 
 const App = () => {
+  if (!localStorage.getItem("username")) {
+    return <LoginForm />;
+  }
   return (
     <div>
       <ChatEngine
         height="100vh"
         projectID="31a09468-01ef-4f07-8539-6eaf1a7eac81"
-        userName="Jack"
-        userSecret="amitjhacse"
+        userName={localStorage.getItem("username")}
+        userSecret={localStorage.getItem("password")}
         renderChatFeed={(chatAppProps) => <ChatFeeds {...chatAppProps} />}
       />
     </div>
--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import axios from "axios";
 
-const projectID = "1b7801d6-8a66-4be4-a442-89219d833dfc";
+const projectID = "31a09468-01ef-4f07-8539-6eaf1a7eac81";
 
 const Modal = () => {
   const [username, setUsername] = useState("");
